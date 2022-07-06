1d69adfd7d9c7a432924f2f939634392ae66f4dc
go-compile <97609133+go-compile@users.noreply.github.com>
2022-07-06 17:28:20 +0100

    add: example connect

---
+++ example/ping/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/go-compile/gorevolt"
+)
+
+func main() {
+	token, exists := os.LookupEnv("revolt_token")
+	if !exists {
+		log.Fatal("set your token as the ENV revolt_token")
+	}
+
+	client := gorevolt.New(token)
+	close := make(chan struct{})
+
+	client.Register(func(c *gorevolt.Client, startup time.Duration) {
+		fmt.Printf("[CONNECTED] [USER: %s]\n", c.User.Username)
+	})
+
+	client.Register(func(c *gorevolt.Client, m *gorevolt.Message) {
+		fmt.Printf("[NEW MESSAGE] [USER: %s] [SERVER: %s] [CHANNEL: %s] %q\n", m.AuthorID, m.Server().Name, m.Channel.Name, m.Content)
+
+		author, err := m.Author()
+		if err != nil {
+			log.Println(err)
+		}
+
+		fmt.Println(author.Username)
+
+		// close and finish test
+		// close <- struct{}{}
+	})
+
+	if err := client.Connect(); err != nil {
+		log.Fatal(err)
+	}
+
+	<-close
+	client.Close()
+}
